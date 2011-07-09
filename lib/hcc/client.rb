3e659c2bb445d6c39ddc05f98cffc6dd3a64bd43
Chetan Sarva <chetan@pixelcop.net>
2011-07-09 16:22:22 -0400

    moved client code into Client class; fixed some error handling

--- lib/hcc/client.rb
+++ lib/hcc/client.rb
@@ -11,42 +11,78 @@ class Cinatra
 end
 
 HADOOP = HCC::Hadoop.new
-@hadoop = HADOOP
 
-puts "hadoop command-line client v#{HCC::VERSION}"
-puts "type 'help' for help"
+module HCC
 
-command 'ls' do |arg|
-    ret = @hadoop.ls(arg)
-    if ret.error? then
-        puts "error running command: #{ret.cmd} - #{ret.stderr}"
-        return
-    end
-    c = 0
-    ret.stdout.split(/\n/).each do |f|
-        c += 1
-        if c == 1
-            puts f
-            next
+    class Client
+
+        def start!
+
+            @hadoop = HADOOP
+
+            register_commands()
+
+            puts "hadoop command-line client v#{HCC::VERSION}"
+            puts "type 'help' for help"
+
         end
-        # TODO: handle filenames with spaces (possibly multiple spaces)
-        s = f.split(/\s+/)
-        (perm, repl, user, group, size, date, time) = s
-        file = s[7..s.length].join(" ")
-        file.sub!(@hadoop.path + "/", '')
-        if perm =~ /^d/ then
-            file = file.colorize(:blue)
+
+        def cmd_ls(arg)
+
+            ret = @hadoop.ls(arg)
+            if ret.error? then
+                if ret.stderr =~ /No such file or directory/ then
+                    puts ret.stderr
+                else
+                    puts "error running command: #{ret.cmd} - #{ret.stderr}"
+                end
+                return
+            end
+            c = 0
+            ret.stdout.split(/\n/).each do |f|
+                c += 1
+                if c == 1
+                    puts f
+                    next
+                end
+                # TODO: handle filenames with spaces (possibly multiple spaces)
+                s = f.split(/\s+/)
+                (perm, repl, user, group, size, date, time) = s
+                file = s[7..s.length].join(" ")
+                file.sub!(@hadoop.path + "/", '')
+                if perm =~ /^d/ then
+                    file = file.colorize(:blue)
+                end
+                puts [perm, repl, user, group, size, date, time, file].join("\t")
+
+            end
+
         end
-        puts [perm, repl, user, group, size, date, time, file].join("\t")
 
-    end
+        def cmd_cd(arg)
+            ret = @hadoop.cd(arg)
+            if ret.error? and ret.stderr =~ /No such file or directory/ then
+                puts ret.stderr
+            end
+        end
 
-end
 
-command 'cd' do |arg|
-    ret = @hadoop.cd(arg)
-    if ret.error? then
-        puts "invalid path: arg"
-        return
-    end
-end
+        private
+
+        def register_commands
+
+            command 'ls' do |arg|
+                cmd_ls(arg)
+            end
+
+            command 'cd' do |arg|
+                cmd_cd(arg)
+            end
+
+        end
+
+    end # Client
+
+end # HCC
+
+HCC::Client.new.start!
