ff997f9e6d7e475ff2e4edef123b7661eac08dcd
Chetan Sarva <chetan@pixelcop.net>
2012-12-07 13:48:11 -0500

    fixed load order

--- lib/hcc.rb
+++ lib/hcc.rb
@@ -1,11 +1,11 @@
 
 module HCC
-    VERSION = 0.1
+    VERSION = 0.2
 end
 
-require File.join(File.dirname(__FILE__), "hcc", "shell")
 require File.join(File.dirname(__FILE__), "hcc", "exec")
-require File.join(File.dirname(__FILE__), "hcc", "hadoop")
-require File.join(File.dirname(__FILE__), "hcc", "client")
 require File.join(File.dirname(__FILE__), "hcc", "command")
 require File.join(File.dirname(__FILE__), "hcc", "capture_output")
+require File.join(File.dirname(__FILE__), "hcc", "hadoop")
+require File.join(File.dirname(__FILE__), "hcc", "shell")
+require File.join(File.dirname(__FILE__), "hcc", "client")
